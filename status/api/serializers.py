2bbbb9f954325846369c2bba8fc662c6a791d0cf
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2020-06-03 20:41:36 +0530

    model setup done
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

---
+++ status/api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from status.models import Status
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = ['user', 'content', 'image']
+
+
+    '''
+    in rest framework validate<filedname> function can be used to write 
+    custom validations for every field. the arg value stores the value passed in the field.
+    '''
+    # def validate_content(self, value):
+    #     if len(value)  < 10:
+    #         raise serializers.ValidationError("Atleast write somethining dude")
+    
+    def validate(self, data):
+        content = data.get('content', None)
+        if content == '':
+            content = None
+
+        image = data.get('image')
+        if content is None and image is None:
+            raise serializers.ValidationError("Content or image is required")
+
+        return data
---
+++ status/models.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import models
+
+def upload_status_image(instance, filename):
+    return "status/{user}/{filename}".format(instance.user, instance.filename)
+
+class Status(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.CharField(max_length=10000, blank=True, null=True)
+    image = models.ImageField(upload_to=upload_status_image, blank=True, null=True)
+    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.content)[:50]
+
+    class Meta:
+        verbose_name = 'Status Post'
+        verbose_name_plural = 'Status Posts'
