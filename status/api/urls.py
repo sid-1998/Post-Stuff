3861d0056117bb30a1701a5bbfef2d05083f9de1
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2020-06-04 20:36:41 +0530

    added some validation
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- status/api/urls.py
+++ status/api/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', StatusListAPIView.as_view()),
     path('create/', StatusCreateAPIView.as_view()),
     path('<int:pk>/', StatusDetailAPIView.as_view()),
-    path('<int:pk>/update', StatusUpdateAPIView.as_view()),
-    path('<int:pk>/delete', StatusDeleteAPIView.as_view()),
+    path('<int:pk>/update/', StatusUpdateAPIView.as_view()),
+    path('<int:pk>/delete/', StatusDeleteAPIView.as_view()),
 
     path('<str:name>', UserStatusAPIView.as_view()),
 
