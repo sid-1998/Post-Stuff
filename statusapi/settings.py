30183ce8018b9b2abea33e93676c2cf3a4867f19
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2020-06-20 21:09:14 +0530

    testing
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- statusapi/settings.py
+++ statusapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '*', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'https://poststuff.herokuapp.com/']
 
 
 # Application definition
